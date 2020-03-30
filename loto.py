a801ef3f123bf4aeac96e58182deb4c08dc339c6
Gman <mrgman11@yandex.ru>
2020-03-30 06:25:25 +0300

    bug fixes

--- loto.py
+++ loto.py
@@ -28,46 +28,40 @@ def decorate_robot(f):
 class CardGame:
 
     @staticmethod
-    def bochonok_number(spisok):
+    def barrel_number(checklist):
 
-        new_num = randrange(1, len(spisok), 1)
+        new_num = randrange(1, len(checklist), 1)
 
-        return spisok.pop(new_num)
+        return checklist.pop(new_num)
 
     @staticmethod
-    def win(spisok):
+    def win(checklist):
         i = 0
         for x in range(3):
             for y in range(9):
-                if str(spisok[x][y]).isdigit() is True:
+                if str(checklist[x][y]).isdigit() is True:
                     i += 1
         if i == 0:
             return "win"
 
 
-class Player():
+class Player:
     @staticmethod
     def generate_card():
 
-        # generate mass structure
+        # создаем структуру билета лото
 
         dx = 9
         dy = 3
         structure = [[0 for x in range(dx)] for y in range(dy)]
 
-        # print(structure)
-
-        # generate random values
+        # генерируем рандомные значения бочонков в билете
 
         random_values = []
         while len(random_values) < 16:
             new_num = randrange(1, 90, 1)
             if new_num not in random_values:
                 random_values.append(new_num)
-
-        # print(random_values)
-
-        # get 3 lists_indexes for 3 rows
 
         numbers_1 = []
         while len(numbers_1) < 5:
@@ -87,19 +81,13 @@ class Player():
             if new_num not in numbers_3:
                 numbers_3.append(new_num)
 
-        # print(numbers_1)
-        # print(numbers_2)
-        # print(numbers_3)
         numbers_result = numbers_1 + numbers_2 + numbers_3
-
-        # print(numbers_result)
 
         for x in range(3):
             for y in range(9):
                 index = str(x) + str(y)
                 if index[0] == "0":
                     index = index[1]
-                # print(index)
 
                 if int(index) in numbers_result:
 
@@ -114,12 +102,9 @@ class Player():
         self.card = self.generate_card()
 
     def pretty_print_generate(self):
-        print("==========This is your card=============")
-        # print(f'============== vipala cifra x ==============')
+        print("==========Это ваша карта=============")
         for x in range(3):
             print(self.card[x])
-
-    # print("======= Zacherknut cifru??   y/n =======================")
 
     def _continue_game(self, num):
         check = False
@@ -133,7 +118,6 @@ class Player():
 
             return True
         else:
-            # print(f'game is over(continue)({num})')
             return False
 
     def _delete_card(self, num):
@@ -148,13 +132,12 @@ class Player():
 
             return self.card
         else:
-            # print(f'game is over(delete)({num})')
             return False
 
 
 class Human(Player):
-    def __init__(self):
-        self.card = self.generate_card()
+
+    Player.card = Player.generate_card()
 
     @decorate_human
     def human_action_del(self, num):
@@ -167,18 +150,17 @@ class Human(Player):
 
 class Computer(Player):
 
-    def __init__(self):
-        self.card = self.generate_card()
+    Player.card = Player.generate_card()
 
     def robot_delete(self, num):
-        if(self._delete_card(num)) is not False:
-            print("robot choise - delete")
+        if(self._delete_card(num)):
+            print("компьютер решил зачеркнуть бочонок")
         return self._delete_card(num)
 
     @decorate_robot(robot_delete)
     def action_robo(self, num):
-        if(self._continue_game(num)) is not False:
-            print("robot choise - continue")
+        if(self._continue_game(num)):
+            print("компьютер решил продолжить игру")
         return self._continue_game(num)
 
 
@@ -186,29 +168,29 @@ human_1 = Human()
 robot_1 = Computer()
 card_1 = CardGame
 
-print("==========Welcome to game===============")
-spisok_bochonkov = [x for x in range(90)]
+print("==========Добро пожаловать в игру Лото!===============")
+list_barrels = [x for x in range(90)]
 
 while True:
-    bochonok_1 = card_1.bochonok_number(spisok_bochonkov)
+    barrel_1 = card_1.barrel_number(list_barrels)
     human_1.pretty_print_generate()
-    print(f"========vipalo chislo {bochonok_1}============")
-    answer = input("Cut or continue? y/n ?")
+    print(f"========Выпало число {barrel_1}============")
+    answer = input("Зачеркнуть или продолжить? y/n ?")
     print("==============================================")
-    robot_1.action_robo(bochonok_1)
+    robot_1.action_robo(barrel_1)
     if answer == "y":
-        if human_1.human_action_del(bochonok_1) != "error":
+        if human_1.human_action_del(barrel_1) != "error":
             if card_1.win(human_1.card) == "win":
-                print("you are win!!!")
+                print("Ура, вы победили!!!")
                 break
             continue
         else:
-            print("game is over!!!")
+            print("Конец игры!!!")
             break
     elif answer == "n":
-        if human_1.human_action_cont(bochonok_1) != "error":
+        if human_1.human_action_cont(barrel_1) != "error":
             continue
         else:
-            print(human_1.human_action_cont(bochonok_1))
-            print("game is over!!!")
+            print(human_1.human_action_cont(barrel_1))
+            print("Конец игры!!!")
             break
